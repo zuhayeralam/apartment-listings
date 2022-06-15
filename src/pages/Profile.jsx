3b7a38c96e37129f068c4e6206f8242e3f02fe6c
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-06-15 17:24:04 +1000

    Uploader can see listings and delete them
    The uploader can see their listings in their profile screen and delete them

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,28 +1,68 @@
-import { useState, useEffect } from 'react'
-import { getAuth, updateProfile } from 'firebase/auth'
-import { updateDoc, doc } from 'firebase/firestore'
-import { db } from '../firebase.config'
-import { useNavigate, Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
-import homeIcon from '../assets/svg/homeIcon.svg'
+import { useState, useEffect } from 'react';
+import { getAuth, updateProfile } from 'firebase/auth';
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import ListingItem from '../components/ListingItem';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
 
 function Profile() {
-  const auth = getAuth()
-  const [changeDetails, setChangeDetails] = useState(false)
+  const auth = getAuth();
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
-  })
+  });
 
-  const { name, email } = formData
+  const { name, email } = formData;
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings');
+
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      );
+
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   const onLogout = () => {
-    auth.signOut()
-    navigate('/')
-  }
+    auth.signOut();
+    navigate('/');
+  };
 
   const onSubmit = async () => {
     try {
@@ -30,26 +70,37 @@ function Profile() {
         // Update display name in fb
         await updateProfile(auth.currentUser, {
           displayName: name,
-        })
+        });
 
         // Update in firestore
-        const userRef = doc(db, 'users', auth.currentUser.uid)
+        const userRef = doc(db, 'users', auth.currentUser.uid);
         await updateDoc(userRef, {
           name,
-        })
+        });
       }
     } catch (error) {
-      console.log(error)
-      toast.error('Could not update profile details')
+      console.log(error);
+      toast.error('Could not update profile details');
     }
-  }
+  };
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
-  }
+    }));
+  };
+
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingId));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId
+      );
+      setListings(updatedListings);
+      toast.success('Successfully deleted listing');
+    }
+  };
 
   return (
     <div className='profile'>
@@ -66,8 +117,8 @@ function Profile() {
           <p
             className='changePersonalDetails'
             onClick={() => {
-              changeDetails && onSubmit()
-              setChangeDetails((prevState) => !prevState)
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
             }}
           >
             {changeDetails ? 'done' : 'change'}
@@ -100,9 +151,25 @@ function Profile() {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt='arrow right' />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your Listings</p>
+            <ul className='listingsList'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
