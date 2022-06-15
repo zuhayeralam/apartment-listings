39e4d44894000286578fde91c5acf96ffc1a5f00
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-06-15 15:41:55 +1000

    Added leaflet js to show map for single listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,6 +5,7 @@ import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 function Listing() {
   const [listing, setListing] = useState(null)
@@ -91,6 +92,26 @@ function Listing() {
         <p className='listingLocationTitle'>Location</p>
 
         {/* MAP */}
+
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
