6c27289c79e4f5c94bfeb891aeabb4d0aa40c21c
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-08-04 12:31:21 +1000

    listing and listing item

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
@@ -45,7 +46,9 @@ function Listing() {
   return (
     <main>
       {/* SLIDER */}
-
+      <Helmet>
+        <title>{listing.name}</title>
+      </Helmet>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         slidesPerView={1}
